ff5facaac24006d81c210bdffcbcdc2dfb1c1454
VittorioDauria94 <dauriavittorio94@gmail.com>
2025-10-04 14:46:53 +0200

    feat: snack 2 done

--- js/script.js
+++ js/script.js
@@ -23,7 +23,9 @@ for (let i = 1; i < bikes.length; i++) {
         lessWeightBike = curBike;
     }
 }
-console.log("la bici che pesa di meno è:", lessWeightBike);
+console.log(bikes);
+console.log("la bici che pesa di meno è:", lessWeightBike.nome, "e pesa:", lessWeightBike.peso, "kg");
+
 
 // Snack2
 // Creare un array di oggetti di squadre di calcio.
@@ -31,6 +33,31 @@ console.log("la bici che pesa di meno è:", lessWeightBike);
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const teams = [
+  { nome: "Squadra1", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Squadra2", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Squadra3", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Squadra4", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Squadra5", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Squadra6", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Squadra7", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Squadra8", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Squadra9", puntiFatti: 0, falliSubiti: 0 },
+  { nome: "Squadra10", puntiFatti: 0, falliSubiti: 0 }
+];
+
+const teamsFouls = [];
+for (let i = 0; i < teams.length; i++) {
+    const curTeam = teams[i];
+    curTeam.puntiFatti = generateRandomNumber(1, 50);
+    curTeam.falliSubiti = generateRandomNumber(1, 20);
+    teamsFouls.push({nome: curTeam.nome, falliSubiti: curTeam.falliSubiti})
+}
+
+console.log(teams);
+console.log(teamsFouls);
+
 
 
 // Snack 3 (Bonus)
@@ -46,3 +73,13 @@ console.log("la bici che pesa di meno è:", lessWeightBike);
 
 // const array1 = [1, 2, 3, 4, 5];
 // const array2 = ["a", "b", "c"]; 
+
+
+
+
+//Funzioni
+// Function Random
+function generateRandomNumber(min, max) {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomNumber;
+}
